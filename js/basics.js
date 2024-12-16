c22188d181af67584801dd55ba471440423be28d
nitish272003 <158252059+nitish272003@users.noreply.github.com>
2024-12-17 00:39:38 +0530

    Create basics.js

---
+++ js/basics.js
@@ -0,0 +1,137 @@
+/*
+// variables in JS
+//types of variables in JS are var, let, const
+var a = 10
+var b = 20
+console.log(a+b)
+
+{
+    let a = 10
+    console.log(a)
+}
+
+const a = 10
+console.log(a)
+
+
+//ar are global scope or function scope
+//let are block scope
+//const is declared to constant variables, we can't change
+
+
+var price = 100
+var product = 'book'
+var tax = 50
+console.log(product)
+console.log(price+tax)
+
+var fruit = 'mango'
+var count = 100
+var price = 10
+var total = count*price
+console.log(fruit)
+console.log(total)
+
+//operators are +,-,*,/ in JS
+
+var a = 10
+a++ //a++ or a--, ++a or --a when can use here.
+console.log(a) //it will print as 10
+
+//post-increment
+var a = 10
+var b = a++
+console.log(b) //it will print as 10
+console.log(a) // it will print as 11
+
+//pre-increment
+var a = 10
+var b = ++a
+console.log(b) //it will print as 11
+
+//Datatype in JS
+
+//primitive Datatype are:
+console.log(typeof(10)) //numbers
+console.log(typeof("orange")) //Strings
+console.log(typeof(null)) //undefined or null
+console.log(typeof(true)) //boolean
+
+//JS function is a block of code designed to perform a particular task.
+
+/*
+    syntax:
+    function <function name>(){}
+*/
+/*
+function demo()
+{
+    console.log("Hi Developers")
+}
+demo()
+
+var a = 10
+var b = 20
+
+function add()
+{
+    console.log(a+b)
+}
+add()
+
+
+var factor = 'vijay'
+var fplayer = 'dhoni'
+var fgame = 'cricket'
+
+function favorite()
+{
+    console.log("fav actor:"+ factor)
+    console.log("fav player:"+ fplayer)
+    console.log("fav game:"+ fgame)
+}
+
+favorite()
+
+function area(length,breadth)
+{
+    console.log('length:'+length)
+    console.log('breadth:'+breadth)
+    console.log('Area:'+ length*breadth)
+}
+
+area(10,50)
+
+
+//if else in JS
+
+var a = 10
+var b = 10
+
+if (a = b){
+    console.log("ok")
+}else{
+    console.log("not ok")
+}
+
+
+//for loop in JS
+
+for(a=1;a<=10;a=a+1){
+    console.log("Nitish") //it will print nitish for 10 times
+}
+
+for(a=1;a<=10;a=a+1){
+    console.log(a) //it will print upto number 10
+}
+
+for(a=1;a<=10;a=a+1){
+    console.log(a++) //it will print odd number till number 10
+}
+
+for(a=0;a<=10;a=a+1){
+    console.log(a++) //it will print even number till number 10
+}
+
+*/
+
